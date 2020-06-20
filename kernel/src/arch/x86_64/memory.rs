16753bf98f443bdcacc4f0536ceb5e0bcd31a5fd
zoniony <1173900767@qq.com>
2020-06-21 02:13:36 +0800

    FrameAllocator

--- kernel/src/arch/x86_64/memory.rs
+++ kernel/src/arch/x86_64/memory.rs
@@ -1,9 +1,15 @@
-
+use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
 use x86_64::{
     structures::paging::*,
     PhysAddr,
     VirtAddr,
 };
+
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
 
 pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
     
@@ -15,6 +21,60 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static
     let page_table_ptr: * mut PageTable = virt.as_mut_ptr();
 
     &mut *page_table_ptr
+}
+
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flag = Flags::PRESENT | Flags::WRITABLE;
+    let map_to_result = unsafe {
+        mapper.map_to(page, frame, flag, frame_allocator)
+    };
+    map_to_result.expect("map_to failed").flush();
+
+}
+
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        None
+    }
+}
+
+pub struct BootinfoFrameAllocator {
+    memory_map: &'static MemoryMap,
+    next: usize,
+}
+
+impl BootinfoFrameAllocator {
+    pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
+        BootinfoFrameAllocator {
+            memory_map,
+            next: 0,
+        }
+    }
+
+    fn usable_frame(&self) -> impl Iterator<Item = PhysFrame> {
+        let regions = self.memory_map.iter();
+        let usable_regions = regions.filter(|r| r.region_type == MemoryRegionType::Usable);
+        let addr_ranges = usable_regions.map(|r| r.range.start_addr()..r.range.end_addr());
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+    }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootinfoFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        let frame = self.usable_frame().nth(self.next);
+        self.next += 1;
+        frame   
+    }
 }
 
 pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -9,7 +9,6 @@ use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
 
 
-
 entry_point!(kernel_main);  
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
@@ -18,28 +17,20 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     kernel::init();
 
-    use kernel::arch::memory::translate_addr;
-    use x86_64::VirtAddr;
-    use x86_64::structures::paging::PageTable;
+    use kernel::arch::memory::{self, BootinfoFrameAllocator};
+    use x86_64::{structures::paging::Page, VirtAddr};
     
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
 
-    let addresses = [
-        // the identity-mapped vga buffer page
-        0xb8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // virtual address mapped to physical address 0
-        boot_info.physical_memory_offset,
-    ];
+    // map an unused page
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
-        println!("{:?} -> {:?}", virt, phys);
-    }
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
 
     #[cfg(test)]
     test_main();
@@ -60,3 +51,4 @@ fn panic(info: &PanicInfo) -> ! {
     kernel::test_panic_handler(info)
 }
 //0x7ea7  load_next_kernel_block_from_disk
+//0x18000001000 p4_table
